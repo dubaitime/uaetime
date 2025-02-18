9e520d9936703706a52b7d24b2588bdaac995d3e
Muhamad Sarif <adubaitime@gmail.com>
2025-02-19 06:31:27 +0700

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,7 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <meta name="google-site-verification" content="Fpr07LDWU9QkPGDn1OHt8MGCxb-lfwMZU4xOOzmY7Vg" />
     </Head>
   );
 }
